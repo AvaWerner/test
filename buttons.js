8d5e509bd02932c9c148ac6a22657efa8690ba5a
AvaWerner <114275002+AvaWerner@users.noreply.github.com>
2025-11-30 22:43:27 -0800

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,19 @@
+
+//Answer Options
+let buttons = document.querySelectorAll(".buttons");
+let rightAnswer = document.getElementById("CorrectAnswer");
+let wrongAnswer1 = document.getElementById("wrongAnswer1");
+let wrongAnswer2 = document.getElementById("wrongAnswer2");
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        rightAnswer.style.backgroundColor = "lightgreen";
+        wrongAnswer1.style.backgroundColor = "red";
+        wrongAnswer2.style.backgroundColor = "red";
+    })
+}
+
+
+
+
+
+
---
+++ menu.js
@@ -0,0 +1,9 @@
+
+//Menu Play Button
+let play = document.getElementById("play");
+play = play.addEventListener("click", nextPage);
+function nextPage(event){
+    window.location.assign("page1.html");
+}
+
+
