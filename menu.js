35756f00a96b2b80449f1fbeeea076a389ea3437
AvaWerner <114275002+AvaWerner@users.noreply.github.com>
2025-12-02 15:01:31 -0800

    Delete menu.js

--- menu.js
+++
@@ -1,9 +0,0 @@
-
-//Menu Play Button
-let play = document.getElementById("play");
-play = play.addEventListener("click", nextPage);
-function nextPage(event){
-    window.location.assign("page1.html");
-}
-
-
